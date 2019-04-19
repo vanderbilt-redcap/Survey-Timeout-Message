741f55b25478c09eaef079f6d1874d35df41c155
Carl Reed <carl.w.reed@vumc.org>
2019-04-19 09:06:00 -0500

    added repeatable sub-settings to allow admin to customize message for multiple surveys on a project

--- SurveyTimeoutMessage.php
+++ SurveyTimeoutMessage.php
@@ -2,16 +2,32 @@
 namespace Vanderbilt\SurveyTimeoutMessage;
 
 class SurveyTimeoutMessage extends \ExternalModules\AbstractExternalModule {
-	function redcap_every_page_top() {
-		if (PAGE == "surveys/index.php") {
-			$msg = $this->getProjectSetting('timeout-message');
-			echo <<<EOF
+	function redcap_every_page_top($project_id) {
+		global $surveys_enabled;
+		global $survey_enabled;
+		global $date_deleted;
+		global $status;
+		if (PAGE == 'surveys/index.php' && (!$surveys_enabled || $survey_enabled < 1 || $date_deleted != '' || $status == 2 || $status == 3)) {
+			// see if there is a custom timeout message configured for this expired survey
+			global $form_name;
+			$settings = $this->getProjectSettings('surveys');
+			foreach($settings['survey-name']['value'] as $i => $name) {
+				if ($name == $form_name) {
+					$customSurveyTimeoutMessage = $settings["timeout-message"]["value"][$i];
+					break;
+				}
+			}
+			
+			// inject js to replace 2nd div html
+			if (isset($customSurveyTimeoutMessage)) {
+				echo <<<EOF
 	<script type='text/javascript'>
 		$(function() {
-			$("body div:nth-child(2)").html("$msg")
+			$("body div:nth-child(2)").html("$customSurveyTimeoutMessage")
 		});
 	</script>
 EOF;
+			}
 		}
 	}
 }
